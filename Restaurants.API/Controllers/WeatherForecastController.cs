cc31a613d4ce69973d88a9ebdafb048e411c9c88
kon-amp <kon.ampatzidis@gmail.com>
2025-07-17 17:06:52 +0300

    chore: simple routing and model binding logic for more endpoints

--- Restaurants.API/Controllers/WeatherForecastController.cs
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -4,21 +4,30 @@ namespace Restaurants.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WeatherForecastController : ControllerBase
-{
+public class WeatherForecastController : ControllerBase {
     private readonly ILogger<WeatherForecastController> _logger;
     private readonly IWeatherForecastService _weatherForecastService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
-    {
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService) {
         _logger = logger;
         _weatherForecastService = weatherForecastService;
     }
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("{take}")]
+    public IEnumerable<WeatherForecast> Get([FromQuery] int max,[FromRoute] int take)
     {
         var result = _weatherForecastService.Get();
         return result;
+    }
+
+    [HttpGet("currentDay")]
+    public WeatherForecast GetCurrentDayForecast() {
+        var result = _weatherForecastService.Get().First();
+        return result;
+    }
+
+    [HttpPost]
+    public string Hello([FromBody] string name) {
+        return $"Hello {name}";
     }
 }
