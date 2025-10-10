6b925d9a75333b42e79c5c3e6e5faef2eab8e2e0
kon-amp <kon.ampatzidis@gmail.com>
2025-10-10 15:49:53 +0300

    add seed data, data that are loading to the database before even the application starts

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,59 @@
+﻿
+using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders;
+internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSeeder {
+    public async Task Seed() {
+        if (await dbContext.Database.CanConnectAsync()) {
+            if (!dbContext.Restaurants.Any()) {
+                var restaurants = GetRestaurants();
+                dbContext.Restaurants.AddRange(restaurants);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+
+    private IEnumerable<Restaurant> GetRestaurants() {
+        List<Restaurant> restaurants = [
+            new() {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquarter",
+                ContactEmail = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes = [
+                    new(){
+                        Name = "Nashville Hot Chicken",
+                        Description = "Nashville Hot Chicken (10 pcs.)",
+                        Price = 10.30M,
+                    },
+                    new(){
+                        Name = "Chicken Nuggets",
+                        Description = "Chicken Nuggets (5 pcs.)",
+                        Price = 5.30M,
+                    }
+                ],
+                Address = new () {
+                    City = "London",
+                    Street = "Cork St 5",
+                    PostalCode = "WC2N 5DU"
+                }
+            },
+            new() {
+                Name = "McDonald",
+                Category = "Fast Food",
+                Description = "McDonald's Corporation, incorporated o December 21, 1964, operates",
+                ContactEmail = "contact@mcdonald.com",
+                HasDelivery = true,
+                Address = new () {
+                    City = "London",
+                    Street = "Boots 193",
+                    PostalCode = "W1F 8SR"
+                }
+            }
+        ];
+
+        return restaurants;
+    }
+}
