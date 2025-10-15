74e40ecec4e896a0a3328e5ff322b4be4eaf4067
kon-amp <kon.ampatzidis@gmail.com>
2025-10-15 11:36:46 +0300

    simple model validation

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -4,6 +4,10 @@ using Restaurants.Application.Restaurants.Dtos;
 
 namespace Restaurants.API.Controllers;
 
+// The [ApiController] attribute automatically validates incoming requests 
+// based on the type of the request model.
+// It also assumes parameters are passed from the request body ([FromBody]) 
+// if no other source is specified.
 [ApiController]
 [Route("api/restaurants")]
 public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase {
@@ -21,6 +25,13 @@ public class RestaurantsController(IRestaurantsService restaurantsService) : Con
 
     [HttpPost]
     public async Task<IActionResult> CreateRestaurant([FromBody]CreateRestaurantDto createRestaurantDto) {
+        #region Manual request validation (used if [ApiController] is not applied)
+        // If the [ApiController] attribute isn’t used, we need to manually check 
+        // whether the ModelState is valid before processing the request.
+        //if (!ModelState.IsValid) {
+        //    return BadRequest(ModelState);
+        //}
+        #endregion
         int id = await restaurantsService.Create(createRestaurantDto);
         return CreatedAtAction(nameof(GetById), new { id }, null);
     }
