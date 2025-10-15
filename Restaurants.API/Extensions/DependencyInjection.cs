e3d37ddac77829a0edb30ae538dbe0000aabdc46
kon-amp <kon.ampatzidis@gmail.com>
2025-10-15 14:36:46 +0300

    small refactor

--- Restaurants.API/Extensions/DependencyInjection.cs
+++ Restaurants.API/Extensions/DependencyInjection.cs
@@ -5,16 +5,16 @@ using Restaurants.API.Filters;
 namespace Restaurants.API.Extensions;
 public static class DependencyInjection {
     public static IServiceCollection AddPresentationLayer(this IServiceCollection services, IConfiguration configuration) {
-        services.AddControllers(options =>
-        {
-            options.Filters.Add<ValidationFilter>();
-        });
-
-        // Disable automatic [ApiController] model validation (DataAnnotations)
-        // so you can handle validation manually or via FluentValidation.
-        services.Configure<ApiBehaviorOptions>(options => {
-            options.SuppressModelStateInvalidFilter = true;
-        });
+        services
+            .AddControllers(options => {
+                options.Filters.Add<ValidationFilter>();
+            })
+            .ConfigureApiBehaviorOptions(options => {
+                // Disable automatic [ApiController] model validation (DataAnnotations)
+                // so you can handle validation manually or via FluentValidation.
+                options.SuppressModelStateInvalidFilter = true;
+            });
+       
 
         services.AddSwaggerGen(c => {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPISample", Version = "v1" });
