f83ea722f56d76f25316bb16399d1140f3115161
kon-amp <kon.ampatzidis@gmail.com>
2026-02-19 15:22:52 +0200

    add validators for assing and unassing user role

---
+++ Restaurants.Application/User/Commands/AssignUserRole/Validators/AssignUserRoleCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.User.Commands.AssignUserRole.Validators; 
+public class AssignUserRoleCommandValidator : AbstractValidator<AssignUserRoleCommand> {
+    public AssignUserRoleCommandValidator() {
+        RuleFor(user => user.UserEmail)
+            .NotEmpty()
+            .WithMessage("User email is required.");
+
+        RuleFor(user => user.RoleName)
+            .NotEmpty()
+            .WithMessage("Role name is required.");
+    }
+}
---
+++ Restaurants.Application/User/Commands/UnnasignUserRole/Validators/UnassignUserRoleCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.User.Commands.UnnasignUserRole.Validators; 
+public class UnassignUserRoleCommandValidator : AbstractValidator<UnassignUserRoleCommand> {
+    public UnassignUserRoleCommandValidator() {
+        RuleFor(user => user.UserEmail)
+            .NotEmpty()
+            .WithMessage("User email is required.");
+
+        RuleFor(user => user.RoleName)
+            .NotEmpty()
+            .WithMessage("Role name is required.");
+    }
+}
